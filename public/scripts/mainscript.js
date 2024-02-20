b42658095e0f491807554c5cbd8417d24c092efb
brandon <wingo206bc@gmail.com>
2024-02-20 10:29:38 -0500

    added canvas

--- public/scripts/mainscript.js
+++ public/scripts/mainscript.js
@@ -5,3 +5,13 @@ function brogaButton() {
   let brog = document.getElementById("broga")
   brog.innerHTML = brog.innerHTML + "aoisudhflasykdufhlkj "
 }
+
+
+function updateCanvas() {
+  let canvas = document.getElementById("canvas")
+  let ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#EEEEEE"
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+updateCanvas()
