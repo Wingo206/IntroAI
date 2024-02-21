c1dab8c02d6510b964c178850f98bb2c869af8a0
brandon <wingo206bc@gmail.com>
2024-02-21 12:32:53 -0500

    animation for generating maze

--- public/scripts/mainscript.js
+++ public/scripts/mainscript.js
@@ -1,4 +1,7 @@
 
+const sleepTime = 10;
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
 function drawLine(ctx, x1, y1, x2, y2) {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -7,11 +10,11 @@ function drawLine(ctx, x1, y1, x2, y2) {
 }
 
 // map: 2d array of ints
-// 0 = empty, 1 = wall, 2 = unknown, 3 = start, 4 = end, 5 = open, 6 = closed
+// 0 = empty, 1 = wall, 2 = unknown, 3 = currentPos, 4 = start, 5 = end, 6 = open, 7 = closed
 function updateCanvas(map) {
     let canvas = document.getElementById("canvas");
     let ctx = canvas.getContext("2d");
-    let squareColors = ["#EEEEEE", "#333333", "#999999"];
+    let squareColors = ["#EEEEEE", "#333333", "#999999", "#EE3333"];
     ctx.fillStyle = "#EEEEEE";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     let boxSize = Math.min(canvas.width / map[0].length, canvas.height / map.length);
@@ -37,7 +40,7 @@ function updateCanvas(map) {
 const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
 
 // reads size from the input fields, then generates the maze
-function generateMaze() {
+async function generateMaze() {
     let mazeWidth = Number(document.getElementById("mazeWidth").value);
     let mazeHeight = Number(document.getElementById("mazeHeight").value);
     // array with numbers saying which direction you came from, -1 if unvisited
@@ -46,8 +49,8 @@ function generateMaze() {
     let curX = 0;
     let curY = 0;
     maze[curX][curY] = 0; // went right to get here
-    let endx = mazeWidth - 1;
-    let endy = mazeHeight - 1;
+    let endX = mazeWidth - 1;
+    let endY = mazeHeight - 1;
 
     let count = 1; // initialize as 1 (starting square)
     let totalSquares = mazeWidth * mazeHeight;
@@ -71,10 +74,12 @@ function generateMaze() {
             validDirections.push(dir);
         }
         // if there are no valid direcitons, then backtrack
-        if (validDirections.length == 0) {
+        if (validDirections.length == 0 || (curX == endX && curY == endY)) {
             let dirCameFrom = directions[maze[curX][curY]];
             curX -= dirCameFrom[0];
             curY -= dirCameFrom[1];
+            updateCanvas(getMapFromMaze(maze, curX, curY));
+            await sleep(sleepTime)
             continue;
         }
         // move to a random neighbor
@@ -83,27 +88,36 @@ function generateMaze() {
         curY += pickedDir[1];
         maze[curX][curY] = directions.indexOf(pickedDir);
         count++;
+        // uppdate the visual
+        updateCanvas(getMapFromMaze(maze, curX, curY));
+        await sleep(sleepTime)
     }
 
-    updateCanvas(getMapFromMaze(maze));
+    updateCanvas(getMapFromMaze(maze, curX, curY));
 
 }
 
 // turns map into maze with walls in between cells
-function getMapFromMaze(maze) {
+function getMapFromMaze(maze, curX, curY) {
     let mapW = 2 * maze.length + 1;
     let mapH = 2 * maze[0].length + 1;
     let map = Array.from(Array(mapW), _ => Array(mapH).fill(1));
 
     for (let x = 0; x < maze.length; x++) {
         for (let y = 0; y < maze[0].length; y++) {
+            // empty the path to show where you came from
+            if (maze[x][y] == -1) {
+                continue;
+            }
             // empty out the cell
             map[2 * x + 1][2 * y + 1] = 0;
-            // empty the path to show where you came from
             let dirCameFrom = directions[maze[x][y]];
             map[2 * x + 1 - dirCameFrom[0]][2 * y + 1 - dirCameFrom[1]] = 0;
         }
     }
+    // set current position
+    map[2 * curX + 1][2 * curY + 1] = 3
+    map[mapW - 1][mapH - 2] = 0; // open the exit
     return map;
 }
 
